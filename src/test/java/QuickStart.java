c8a4f2b997a263b412ccf78755714300dc8c92e0
diklaO <dikla.ohayon@experitest.com>
2019-05-21 17:50:07 +0300

    Initial commit

---
+++ src/test/java/QuickStart.java
@@ -0,0 +1,51 @@
+import io.appium.java_client.ios.IOSDriver;
+import io.appium.java_client.ios.IOSElement;
+import io.appium.java_client.remote.IOSMobileCapabilityType;
+import io.appium.java_client.remote.MobileCapabilityType;
+import org.junit.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.ScreenOrientation;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class QuickStart {
+
+    private String accessKey = "eyJ4cC51IjoyMTg0LCJ4cC5wIjoyLCJ4cC5tIjoiTVRVeE1qWTBNRFU0TXpjek9BIiwiYWxnIjoiSFMyNTYifQ.eyJleHAiOjQ2ODI4MzE2ODIsImlzcyI6ImNvbS5leHBlcml0ZXN0In0.p9txXSa_Br32EFSCT8igpEddnh5rzsWTishPlj1-TXg";
+    protected IOSDriver<IOSElement> driver = null;
+    DesiredCapabilities dc = new DesiredCapabilities();
+
+    @Before
+    public void setUp() throws MalformedURLException {
+        dc.setCapability("testName", "Quick Start iOS Native Demo");
+        dc.setCapability("accessKey", accessKey);
+        dc.setCapability("deviceQuery", "@os='ios' and @category='PHONE'");
+        dc.setCapability(MobileCapabilityType.APP, "cloud:com.experitest.ExperiBank");
+        dc.setCapability(IOSMobileCapabilityType.BUNDLE_ID, "com.experitest.ExperiBank");
+        driver = new IOSDriver<>(new URL("https://qacloud.experitest.com/wd/hub"), dc);
+    }
+
+    @Test
+    public void quickStartiOSNativeDemo() {
+        driver.rotate(ScreenOrientation.PORTRAIT);
+        driver.findElement(By.xpath("//*[@id='usernameTextField']")).sendKeys("company");
+        driver.hideKeyboard();
+        driver.findElement(By.xpath("//*[@id='passwordTextField']")).sendKeys("company");
+        driver.findElement(By.xpath("//*[@id='loginButton']")).click();
+        driver.findElement(By.xpath("//*[@id='makePaymentButton']")).click();
+        driver.findElement(By.xpath("//*[@id='phoneTextField']")).sendKeys("0541234567");
+        driver.findElement(By.xpath("//*[@id='nameTextField']")).sendKeys("Jon Snow");
+        driver.findElement(By.xpath("//*[@id='amountTextField']")).sendKeys("50");
+        driver.findElement(By.xpath("//*[@id='countryButton']")).click();
+        driver.findElement(By.xpath("//*[@id='Switzerland']")).click();
+        driver.findElement(By.xpath("//*[@id='sendPaymentButton']")).click();
+        driver.findElement(By.xpath("//*[@id='Yes']")).click();
+    }
+
+    @After
+    public void tearDown() {
+        System.out.println("Report URL: "+ driver.getCapabilities().getCapability("reportUrl"));
+        driver.quit();
+    }
+}
